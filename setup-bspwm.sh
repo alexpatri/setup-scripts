b83bb8bd62510d987fb66fa236b103a7f59623ed
alex <alex@vbox.vbox>
2026-02-07 09:46:40 -0300

    reorganização dos diretórios e adição de wallpaper

--- setup-bspwm.sh
+++ setup-bspwm.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install bspwm sxhkd polybar dunst alacritty xorg
+sudo apt install bspwm sxhkd polybar dunst alacritty feh xorg
 
 mkdir -p $HOME/.config/{bspwm,dunst,polybar,sxhkd}
 
-cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm/
+cp ./config/bspwm/bspwmrc $HOME/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
 cp /etc/polybar/config.ini $HOME/.config/polybar
 cp /etc/xdg/dunst/dunstrc $HOME/.config/dunst/
 
+mkdir $HOME/Images
+
+cp ./wallpaper.png $HOME/Images/
