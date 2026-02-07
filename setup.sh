d8913505a8401e3aabd1812056378ccc6e96fc58
alex <alex@vbox.vbox>
2026-02-07 12:03:09 -0300

    adicionado configurações do alacritty e picom

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install bspwm sxhkd polybar dunst alacritty feh xorg picom
+
+mkdir $HOME/.config
+
+cp -r ./config/bspwm/ $HOME/.config/
+cp -r ./config/alacritty/ $HOME/.config/
+cp -r ./config/picom/ $HOME/.config/
+# cp /usr/share/doc/bspwm/examples/sxhkdrc $HOME/.config/sxhkd/
+# cp /etc/polybar/config.ini $HOME/.config/polybar
+# cp /etc/xdg/dunst/dunstrc $HOME/.config/dunst/
+
+mkdir $HOME/Images
+
+cp ./wallpaper.png $HOME/Images/
